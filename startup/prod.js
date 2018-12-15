b7e5567d098452bd8fa5150fedd09647388ae54c
Robert Norton <spiffspuzzum@users.noreply.github.com>
2018-12-14 18:19:20 -0800

    adding headers to allow CORS

--- startup/prod.js
+++ startup/prod.js
@@ -2,4 +2,10 @@ const helmet = require('helmet');
 
 module.exports = function(app) {
     app.use(helmet());
+    //Allow CORS
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 };
