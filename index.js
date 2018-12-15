ca99b3d98d40dc5695f4864041f20fda7b0ab82d
Robert Norton <spiffspuzzum@users.noreply.github.com>
2018-12-14 18:33:02 -0800

    changed order on index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 require("./startup/config")();
+require('./startup/prod')(app);
 require("./startup/routes")(app);
 require("./startup/database")();
-require('./startup/prod')(app);
 
 //Listen
 const port = process.env.PORT || 3000;
